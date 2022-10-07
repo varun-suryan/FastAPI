1aa55f4a53c91e022cd6bc8ea2e3d9e424a13778
Varun Suryan <vsuryan@kythera.space>
2022-10-07 15:54:42 -0400

    Churn prediction working

---
+++ dependencies.py
@@ -0,0 +1,102 @@
+import copy
+
+import numpy as np
+from xgboost import XGBClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+import base64
+import pandas as pd
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import train_test_split, cross_validate
+
+from sklearn.pipeline import Pipeline
+from sklearn.inspection import permutation_importance
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.compose import ColumnTransformer
+
+from sklearn.preprocessing import StandardScaler
+class classification:
+    def __init__(self):
+        self.models = [LogisticRegression(), DecisionTreeClassifier(), XGBClassifier()]
+
+
+    def convert_to_dataframe(self, response):
+
+        """Converts the response from the API to a pandas dataframe"""
+        # get the binary text
+        decodedBytes = base64.b64decode(response.text.split(',')[1]).split(b'\r\n')
+
+        columns = [column.split('_')[-1] for column in decodedBytes[0].decode('ascii').split(',')]
+        list_of_rows = []
+
+        for row in decodedBytes[1:]:
+            decoded_row = row.decode('ascii').split(',')
+            list_row = []
+            for item in decoded_row:
+                try:
+                    item = float(item)
+                except:
+                    pass
+                list_row.append(item)
+            if len(list_row) == len(columns):
+                list_of_rows.append(list_row)
+
+        return pd.DataFrame(list_of_rows, columns=columns)
+
+    def best_model(self):
+        scoring = ['roc_auc']
+
+        scores_dict = [cross_validate(model, self.X_train, self.y_train.values.ravel(), scoring=scoring) for model in self.models]
+        scores = [item['test_' + scoring[0]].mean() for item in scores_dict]
+
+        return np.argmax(scores)
+
+
+    def feature_importance(self, data_X, target, chosen_model):
+        categorical_columns = data_X.select_dtypes(include=['object', 'category']).columns
+
+        categorical_encoder = OrdinalEncoder(handle_unknown="use_encoded_value", unknown_value=-1)
+
+        preprocessing = ColumnTransformer([("cat", categorical_encoder, categorical_columns),])
+
+        rf = Pipeline([("preprocess", preprocessing), ("classifier", chosen_model),])
+
+        rf.fit(data_X, target)
+        result = permutation_importance(rf, data_X, target, n_repeats=5, n_jobs=2)
+        self.sorted_importances_idx = result.importances_mean.argsort()
+
+        importances = pd.DataFrame(
+            result.importances[self.sorted_importances_idx].T,
+            columns=data_X.columns[self.sorted_importances_idx],
+        )
+        ax = importances.plot.box(vert=False, whis=10)
+        ax.set_title("Churn Prediction")
+        ax.axvline(x=0, color="k", linestyle="--")
+        ax.set_xlabel("Importance in Churn Prediction")
+        ax.figure.tight_layout()
+        ax.grid();
+        ax.figure.savefig('feature_importance.png')
+        return self.sorted_importances_idx
+
+    def train(self, df, target):
+        self.X = df.drop(['customerid', target, 'id'], axis=1)
+        self.y = df[target].replace({'Yes': 1, 'No': 0})
+
+        featureX, self.X_test, featureY, self.y_test = train_test_split(self.X, self.y, test_size=0.2)
+
+        self.X_train, self.y_train = pd.get_dummies(featureX, drop_first=True), pd.get_dummies(featureY, drop_first=True)
+        self.X_test, self.y_test = pd.get_dummies(self.X_test, drop_first=True), pd.get_dummies(self.y_test, drop_first=True)
+
+        self.X_train, self.X_test = StandardScaler().fit_transform(self.X_train), StandardScaler().fit_transform(self.X_test)
+
+        index_best_model = self.best_model()
+
+        inference_model = self.models[index_best_model]
+        importance_model = copy.deepcopy(inference_model)
+
+        inference_model.fit(self.X_train, self.y_train.values.ravel())
+        self.feature_importance(featureX, featureY.values.ravel(), importance_model)
+        return [{"Best Model is": "{}".format(inference_model)}, classification_report(self.y_test, inference_model.predict(self.X_test), output_dict=True)]
+
--- main.py
+++ main.py
@@ -1,129 +1,55 @@
-import pickle
-from io import BytesIO
-
-from fastapi import FastAPI
+import requests
 import uvicorn
-import sys
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report
-from flaml import AutoML
+from fastapi import FastAPI, APIRouter
 from pydantic import BaseModel
-import numpy as np
-# Creating FastAPI instance
+from fastapi.responses import FileResponse
+from dependencies import classification
+from typing import List
+
 app = FastAPI()
 
-# Creating class to define the request body
-# and the type hints of each attribute
-class request_body(BaseModel):
-    age: int
-    Ca: int
-    Chol: int
-    Cp: int
-    Exang: int
-    Fbs: int
-    OldPeak: float
-    RestEcg: int
-    sex: int
-    Slope: int
-    TrestBps: float
-    Thal: int
-    Thalac: int
-
 class request_body_train(BaseModel):
-    target_col: str
-    train_time: int
-
-# load data
-import requests
-
-url = "https://api.ignatius.io/api/report/export?reportId=ti2coyqg1&tableId=2363&exportType=csv&size=-1&tblName=1"
-
-payload={}
+    task_type: str
+    report_id: str
+    table_id: str
+    bearer_token: str
+    target: str
 
 
-headers={"Authorization": "Bearer X3p-Hum47YMiY8dBEw-OsQpSnVPcZRFdqtSRpx9eEdY"}
-response = requests.request("GET", url, headers=headers, data=payload)
+class pipeline:
+    def __init__(self):
+        self.router = APIRouter()
+        self.router.add_api_route('/', self.home, methods=['GET'])
+        self.router.add_api_route('/train', self.train, methods=['POST'], response_model=List[dict])
+        self.router.add_api_route('/results', self.results, methods=['GET'])
 
-print(response.text.encode('utf8'))
-# dt = np.dtype([('a', 'i4'), ('b', 'i4'), ('c', 'i4'), ('d', 'f4'), ('e', 'i4'),
-#                ('f', 'i4'), ('g', 'i4'), ('h', 'i4'), ('i', 'i4'), ('j', 'i4'),
-#                ('k', 'i4'), ('l', 'i4'), ('m', 'i4'), ('n', 'i4')])
+    def home(self):
+        return {"Hello! I am a model that predicts churn. To train me, send a POST request to /train with a target variable. To get my results, send a GET request to /results."}
+    def results(self):
+        # return {self.task.sorted_importances_idx}
+        return FileResponse('feature_importance.png')
 
-# data = np.genfromtxt(response.content, delimiter=',', skip_header=1, dtype=dt)
+    def train(self, data: request_body_train):
+        if data.task_type == 'classification':
+            self.task = classification()
 
-# headers={"Authorization": "Bearer wgxt87joyqsMlB4AHUD9Zd0V3ITdFkEEzM5htGFnDkk"}
-
-
-response = requests.request("GET", url, headers={"Authorization": "Bearer wgxt87joyqsMlB4AHUD9Zd0V3ITdFkEEzM5htGFnDkk"})
-
-# df = pd.read_csv(BytesIO(response.text))
-# print(df)
-
-
-df = pd.read_csv('Table_2358_Report_5350.csv')
-# initialize AutoML
-automl = AutoML()
-
-
-# define X and y
-# Defining path operation for /name endpoint
-@app.post('/train')
-def train(data : request_body_train):
-    target_col = data.target_col
-    X = df.drop(target_col, axis=1)
-    y = df[target_col]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    automl.fit(X_train=X_train, y_train=y_train, time_budget=data.train_time, task='classification', verbose=0)
-    pickle_out = open("automlmodel.pkl", "wb")
-    pickle.dump(automl, pickle_out)
-    pickle_out.close()
-    return {'Model trained successfully. Best model accuracy is ' + str(accuracy_score(y_test, automl.predict(X_test)))}
+        # This is the URL and Bearer token of the API endpoint
+        headers = {"Authorization": "Bearer {}".format(data.bearer_token)}
+        url = "https://api.ignatius.io/api/report/export?reportId={}&tableId={}&exportType=csv&size=-1&tblName=1".format(data.report_id, data.table_id)
+        response = requests.get(url, headers=headers)
+        self.df = self.task.convert_to_dataframe(response)
+        return self.task.train(self.df, data.target)
 
 
+app.include_router(pipeline().router)
 
-# Creating an Endpoint to receive the data
-# to make prediction on.
-@app.post('/predict')
-def predict(data: request_body):
-    # Making the data in a form suitable for prediction
-    test_data = [[
-        data.age,
-        data.Ca,
-        data.Chol,
-        data.Cp,
-        data.Exang,
-        data.Fbs,
-        data.OldPeak,
-        data.RestEcg,
-        data.sex,
-        data.Slope,
-        data.TrestBps,
-        data.Thal,
-        data.Thalac
-    ]]
-    
-    df = pd.DataFrame(test_data, columns=['Age', 'ca Value', 'Chol', 'CP Value', 'exang', 'FBS Value', 'oldpeak',
-                                'Rest CG', 'Sex', 'slope', 'Test BPS', 'thal', 'thalach'])
-
-    # Predicting the Class
-    trained_model = pickle.load(open('automlmodel.pkl', 'rb'))
-    class_idx = trained_model.predict(df)
-    # Return the Result
-    return {'This person has heart disease.'} if class_idx else {'This person does not have heart disease.'}
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
 
 # {
-#   "age": 63,
-#   "Ca": 0,
-#   "Chol": 233,
-#   "Cp": 3,
-#   "Exang": 0,
-#   "Fbs": 1,
-#   "OldPeak": 2.3,
-#   "RestEcg": 0,
-#   "sex": 1,
-#   "Slope": 0,
-#   "TrestBps": 145,
-#   "Thal": 1,
-#   "Thalac": 150
+#   "task_type": "classification",
+#   "report_id": "ti2coyqg1",
+#   "table_id": "2363",
+#   "bearer_token": "Go9L61T0-HYuNPyYhINW3BuHWIBvZbuVnvu7RAAirJ8",
+#   "target": "churn"
 # }
