6b60978c0654951eea0cec64c257b6bbe011b2ad
Varun Suryan <vsuryan@kythera.space>
2022-10-07 18:51:38 -0400

    Generalized code

--- dependencies.py
+++ dependencies.py
@@ -27,12 +27,13 @@ class classification:
         """Converts the response from the API to a pandas dataframe"""
         # get the binary text
         decodedBytes = base64.b64decode(response.text.split(',')[1]).split(b'\r\n')
-
-        columns = [column.split('_')[-1] for column in decodedBytes[0].decode('ascii').split(',')]
+        columns = [column.split('_')[-1] for column in decodedBytes[0].decode('ascii').split(',')][1:]
         list_of_rows = []
 
+        # Start from the next row
         for row in decodedBytes[1:]:
-            decoded_row = row.decode('ascii').split(',')
+            # drop the first column corresponding the data ID
+            decoded_row = row.decode('ascii').split(',')[1:]
             list_row = []
             for item in decoded_row:
                 try:
@@ -42,7 +43,6 @@ class classification:
                 list_row.append(item)
             if len(list_row) == len(columns):
                 list_of_rows.append(list_row)
-
         return pd.DataFrame(list_of_rows, columns=columns)
 
     def best_model(self):
@@ -61,7 +61,11 @@ class classification:
 
         preprocessing = ColumnTransformer([("cat", categorical_encoder, categorical_columns),])
 
-        rf = Pipeline([("preprocess", preprocessing), ("classifier", chosen_model),])
+        # check if there are any categorical variables
+        if categorical_columns.shape[0]:
+            rf = Pipeline([("preprocess", preprocessing), ("classifier", chosen_model),])
+        else:
+            rf = chosen_model
 
         rf.fit(data_X, target)
         result = permutation_importance(rf, data_X, target, n_repeats=5, n_jobs=2)
@@ -72,16 +76,16 @@ class classification:
             columns=data_X.columns[self.sorted_importances_idx],
         )
         ax = importances.plot.box(vert=False, whis=10)
-        ax.set_title("Churn Prediction")
+        ax.set_title("Prediction")
         ax.axvline(x=0, color="k", linestyle="--")
-        ax.set_xlabel("Importance in Churn Prediction")
+        ax.set_xlabel("Importance in Prediction")
         ax.figure.tight_layout()
         ax.grid();
         ax.figure.savefig('feature_importance.png')
         return self.sorted_importances_idx
 
     def train(self, df, target):
-        self.X = df.drop(['customerid', target, 'id'], axis=1)
+        self.X = df.drop([target, 'id'], axis=1)
         self.y = df[target].replace({'Yes': 1, 'No': 0})
 
         featureX, self.X_test, featureY, self.y_test = train_test_split(self.X, self.y, test_size=0.2)
